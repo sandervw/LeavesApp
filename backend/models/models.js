f78223abcd5c8b5ba5427f68f87fa98ee558e3a2
Sander <14879109+sandervw@users.noreply.github.com>
2025-03-25 17:52:06 -0500

    Refactor backend structure: remove utility routes and controller, update template routes, and introduce element service

--- backend/models/models.js
+++ backend/models/models.js
@@ -3,15 +3,6 @@ import { Element } from './elementModel.js';
 
 const Schema = mongoose.Schema;
 
-// Schema for Prompts
-const promptSchema = new Schema ({});
-
-// Schema for Promptchains
-const promptchainSchema = new Schema({
-    type: { type: String, default: 'generic' }, // Structure, Style, Filter; generic if blank
-    prompts: { type: [String] }, // String array containing the prompts to use
-}, {timestamps: true});
-
 // Schema for Templates
 const templateSchema = new Schema({
     children: { type: [String] }, // String array containing the child element IDs
@@ -22,7 +13,6 @@ const templateSchema = new Schema({
 // Types: Story, Act, Chapter, Scene, Blob
 const storynodeSchema = new Schema ({
     content: { type: String }, // Used in blob nodes; contains actual blob, instead of outline data
-    promptchains: { type: [promptchainSchema] }, // Contains prompt chains for AI generation
     isComplete: { type: Boolean, default: false, required: true }, // Used for rendering a finished vs unfinished element
     parent: { type: String }, // String containing parent node
     children: { type: [String] }, // String array containing the child node IDs
@@ -32,9 +22,7 @@ const storynodeSchema = new Schema ({
     archived: { type: Boolean, default: false }, // Used to store whether a story is archived or not
 });
 
-const Prompt = Element.discriminator('prompt', promptSchema);
-const Promptchain = Element.discriminator('promptchain', promptchainSchema);
 const Template = Element.discriminator('template', templateSchema);
 const Storynode = Element.discriminator('storynode', storynodeSchema);
 
-export {Prompt, Promptchain, Template, Storynode};
+export {Template, Storynode};
