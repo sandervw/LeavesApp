8488017e6b2b0e2deb804d534d0d34efcdfea5eb
Sander <14879109+sandervw@users.noreply.github.com>
2025-03-28 04:30:39 -0500

    Refactor context hooks: replace old hooks with new implementations for Templates and Storynodes contexts; update App structure to include context providers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
+import {Routes, Route} from 'react-router-dom';
+import Navbar from '../components/Navbar';
 import { useAuth0 } from '@auth0/auth0-react';
 import LoginButton from '../components/LoginButton.jsx'
 import LogoutButton from '../components/LogoutButton.jsx'
+import { StorynodesContextProvider } from '../context/storynodesContext.jsx';
+import { TemplatesContextProvider } from '../context/templatesContext';
 
 function App() {
   const { isAuthenticated, user, isLoading } = useAuth0();
@@ -9,9 +13,7 @@ function App() {
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div>
+    <div className="App">
         {!isAuthenticated ? (
           <LoginButton/>
         ) : (
@@ -19,11 +21,28 @@ function App() {
             <h1>Welcome, {user.name}</h1>
             <img src={user.picture} alt="Profile" width="100" style={{ borderRadius: '50%' }} />
             <p>Email: {user.email}</p>
+            <Navbar />
             <LogoutButton/>
           </>
         )}
-      </div>
-    </>
+        <div className="content">
+          <StorynodesContextProvider>
+            <TemplatesContextProvider>
+              <Routes>
+                <Route path='/' element={<LogoutButton />} />
+                {/* <Route path='/archive' element={<Archive />} />
+                <Route path='/storydetail' element={<StorynodeDetail />} />
+                <Route path='/templates' element={<Templates />} />
+                <Route path='/prompts' element={<Prompts />} />
+                <Route path='/promptchains' element={<Promptchains />} />
+                <Route path='/templatedetail' element={<TemplateDetail />} />
+                <Route path='/blobdetail' element={<BlobDetail />} />
+                <Route path='/promptchaindetail' element={<PromptchainDetail />} /> */}
+              </Routes>
+            </TemplatesContextProvider>
+          </StorynodesContextProvider>
+        </div>
+    </div>
   )
 }
 
