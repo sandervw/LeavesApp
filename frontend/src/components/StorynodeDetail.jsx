daec17e4832e99908401c6af9a6937a231ac8830
Sander <14879109+sandervw@users.noreply.github.com>
2025-04-07 19:05:32 -0500

    Refactor StorynodeDetail component: Replace apiService with useAPI hook for improved API integration. Streamline data fetching and state management in useEffect. Enhance addChild function to handle child addition more effectively. Update component structure for better readability and maintainability.

--- frontend/src/components/StorynodeDetail.jsx
+++ frontend/src/components/StorynodeDetail.jsx
@@ -7,12 +7,9 @@ import MarkdownText from "./MarkdownText";
 import Droppable from './Droppable';
 import AddSidebar from '../components/AddSidebar';
 import LinkSidebar from '../components/LinkSidebar';
-import apiService from "../services/apiService";
+import useAPI from "../hooks/useAPI";
 import useElementContext from "../hooks/useElementContext";
 import useTemplateContext from "../hooks/useAddableContext";
-
-
-
 
 const StorynodeDetail = () => {
 
@@ -20,26 +17,23 @@ const StorynodeDetail = () => {
     const navigate = useNavigate();
     const { children, element, dispatch: elementDispatch } = useElementContext();
     const { dispatch: templatesDispatch } = useTemplateContext();
+    const apiCall = useAPI();
     const [showModal, setShowModal] = useState(false);
     const [lockWriting, setLockWriting] = useState(false);
-    const [isPending, setIsPending] = useState(true);
 
-    // Fetch the storynode, its childre, and a list of templates that could be added
+    // Fetch the storynode, its children, and a list of templates that could be added
     useEffect(() => {
         const fetchData = async () => {
-            setIsPending(true);
             console.log("useEffect called");
-            const storynode = await apiService.fetchElement('storynodes', location.state);
-            const storynodeSubtype = 'branch';
-            const children = await apiService.fetchChildren('storynodes', storynode._id);
-            const addables = await apiService.fetchElements('templates', 'type=' + storynodeSubtype);
-            await templatesDispatch({ type: 'SET_TEMPLATES', payload: addables });
-            await elementDispatch({ type: 'SET_CHILDREN', payload: children });
+            const storynode = await apiCall('fetchElement', 'storynodes', location.state);
             await elementDispatch({ type: 'SET_ELEMENT', payload: storynode });
-            setIsPending(false);
+            const children = await apiCall('fetchChildren', 'storynodes', location.state);
+            await elementDispatch({ type: 'SET_CHILDREN', payload: children });
+            const addables = await apiCall('fetchElements', 'templates', 'type=branch');
+            await templatesDispatch({ type: 'SET_TEMPLATES', payload: addables });
         };
         fetchData();
-    }, [location.state, elementDispatch, templatesDispatch]);
+    }, [location.state, elementDispatch, templatesDispatch, apiCall]);
 
     // Return to parent element
     const navigateParent = async () => {
@@ -58,12 +52,12 @@ const StorynodeDetail = () => {
             // If the word count is over the word limit, lock the blobs
             if (sumWords > element.wordLimit) setLockWriting(true);
             else setLockWriting(false);
-            apiService.upsertElement('storynodes', { ...element, wordCount: sumWords });
+            apiCall('upsertElement', 'storynodes', { ...element, wordCount: sumWords });
             elementDispatch({ type: 'SET_ELEMENT', payload: { ...element, wordCount: sumWords } });
         }
         // In remove case, need to both remove and delete the child tree
         else if (attr === 'remove') {
-            apiService.deleteElement('storynodes', val);
+            apiCall('deleteElement', 'storynodes', val);
             elementDispatch({ type: 'DELETE_CHILD', payload: val });
             let data2 = await element.children.filter(child => (child !== val && child !== null));
             if (data2.length === 0 && element.type === 'branch') element.type = 'leaf';
@@ -71,7 +65,7 @@ const StorynodeDetail = () => {
         }
         // In add case, need to convert template and all its children to listNodes (do on backend)
         else if (attr === 'add') {
-            const data = await apiService.createFromTemplate(val._id, element._id);
+            const data = await apiCall('createFromTemplate', val._id, element._id);
             if (element.type === 'leaf') element.type = 'branch';
             elementDispatch({ type: 'CREATE_CHILD', payload: data });
             elementDispatch({ type: 'SET_ELEMENT', payload: { ...element, children: [...element.children, data._id] } });
@@ -81,101 +75,114 @@ const StorynodeDetail = () => {
             const name = attr === 'name' ? val : element.name;
             const text = attr === 'text' ? val : element.text;
             const wordLimit = attr === 'wordLimit' ? val : element.wordLimit;
-            apiService.upsertElement('storynodes', { ...element, name, text, wordLimit });
+            apiCall('upsertElement', 'storynodes', { ...element, name, text, wordLimit });
             elementDispatch({ type: 'SET_ELEMENT', payload: { ...element, name, text, wordLimit } });
         }
         console.log(element);
     };
 
+    const addChild = async (method, data) => {
+        if (element.type === 'leaf') element.type = 'branch';
+        await apiCall('upsertElement', 'storynodes', { ...element });
+        let newChild;
+        if(method === 'createFromTemplate') { //Handle adding to parent on backend
+            newChild = await apiCall(method, data._id, element._id);
+            elementDispatch({ type: 'SET_ELEMENT', payload: { ...element } });
+        }
+        else { //Handle adding to parent on frontend
+            newChild = await apiCall(method, 'storynodes', data);
+            await apiCall('upsertElement', 'storynodes', { ...element, children: [...element.children, newChild._id] });
+            elementDispatch({ type: 'SET_ELEMENT', payload: { ...element, children: [...element.children, newChild._id] } });
+        }
+        elementDispatch({ type: 'CREATE_CHILD', payload: newChild});
+    };
+
     // Delete the storynode and navigate back to main list
     const handleDelete = async () => {
-        apiService.deleteElement('storynodes', element._id);
+        apiCall('deleteElement', 'storynodes', element._id);
         if (!element.parent) navigate('/');
         else navigate('/storydetail', { state: element.parent });
     };
 
     const downloadStory = async () => {
-        const data = await apiService.createFile(element._id);
+        const data = await apiCall('createFile', element._id);
         console.log(data);
     };
 
     const toggleArchive = async () => {
-        await apiService.upsertElement('storynodes', { ...element, archived: !element.archived });
+        await apiCall('upsertElement', 'storynodes', { ...element, archived: !element.archived });
         navigate('/');
     };
 
-    return !isPending && (
-        
+    return element && (
         <>
-        <LinkSidebar />
-        <div className="container content">
-            <div className="element detail">
-                <div className="box-buttons">
-                    <button onClick={() => navigateParent()}>
-                        <InlineSVG src="/return.svg" alt="return icon" className="icon" />
-                    </button>
-                    <button onClick={() => downloadStory()}>
-                        <InlineSVG src="/download.svg" alt="download  icon" className="icon" />
-                    </button>
-                    {element.type === 'root' &&
-                        <button onClick={() => toggleArchive()}>
-                            <InlineSVG src={element.archived ? "/unarchive.svg" : "/archive.svg"} alt="archive  icon" className="icon" />
-                        </button>}
-                    <button onClick={() => setShowModal(true)}>
-                        <InlineSVG src="/trashcan.svg" alt="delete icon" className="icon" />
-                    </button>
-                </div>
-                <div className="box-detail">
-                    <h2
-                        contentEditable
-                        suppressContentEditableWarning={true}
-                        id={"name"}
-                        onBlur={(e) => updateStorynode('name', e.target.innerText)}
-                    >{element.name}
-                    </h2>
-                    <p>Type: {element.type}</p>
-                    {element.wordWeight && <div className="wordweight">
-                        <p>Weight: {element.wordWeight}</p>
-                    </div>}
-                    {element.type === 'root'
-                        ? <div>
-                            <p>Word Limit: </p>
-                            <p
-                                contentEditable
-                                suppressContentEditableWarning={true}
-                                onBlur={(e) => updateStorynode('wordLimit', e.target.innerText)}
-                            >{element.wordLimit}</p>
-                        </div>
-                        : <div>
-                            <h3>Word Limit: {element.wordLimit}</h3>
+            <LinkSidebar />
+            <div className="container content">
+                <div className="element detail">
+                    <div className="box-buttons">
+                        <button onClick={() => navigateParent()}>
+                            <InlineSVG src="/return.svg" alt="return icon" className="icon" />
+                        </button>
+                        <button onClick={() => downloadStory()}>
+                            <InlineSVG src="/download.svg" alt="download  icon" className="icon" />
+                        </button>
+                        {element.type === 'root' &&
+                            <button onClick={() => toggleArchive()}>
+                                <InlineSVG src={element.archived ? "/unarchive.svg" : "/archive.svg"} alt="archive  icon" className="icon" />
+                            </button>}
+                        <button onClick={() => setShowModal(true)}>
+                            <InlineSVG src="/trashcan.svg" alt="delete icon" className="icon" />
+                        </button>
+                    </div>
+                    <div className="box-detail">
+                        <h2
+                            contentEditable
+                            suppressContentEditableWarning={true}
+                            id={"name"}
+                            onBlur={(e) => updateStorynode('name', e.target.innerText)}
+                        >{element.name}
+                        </h2>
+                        <p>Type: {element.type}</p>
+                        {element.wordWeight && <div className="wordweight">
+                            <p>Weight: {element.wordWeight}</p>
                         </div>}
-                    {element.type === 'leaf' &&
-                        <div>
-                            <h3>Word count: {element.wordCount}</h3>
-                        </div>}
-                </div>
-                <div className="box-text">
-                    <MarkdownText text={element.text} update={(val) => updateStorynode('text', val)} />
-                </div>
+                        {element.type === 'root'
+                            ? <div>
+                                <p>Word Limit: </p>
+                                <p
+                                    contentEditable
+                                    suppressContentEditableWarning={true}
+                                    onBlur={(e) => updateStorynode('wordLimit', e.target.innerText)}
+                                >{element.wordLimit}</p>
+                            </div>
+                            : <div>
+                                <h3>Word Limit: {element.wordLimit}</h3>
+                            </div>}
+                        {element.type === 'leaf' &&
+                            <div>
+                                <h3>Word count: {element.wordCount}</h3>
+                            </div>}
+                    </div>
+                    <div className="box-text">
+                        <MarkdownText text={element.text} update={(val) => updateStorynode('text', val)} />
+                    </div>
 
-            </div>
-            <div>
-                <Droppable id="droppable" className="droppable" >
-                <h3>Children:</h3>
-                {children && children.map((child) =>
-                (
-                    <Storynode
-                        storynodeData={child}
-                        buttonType='remove'
-                        parentFunction={updateStorynode}
-                        locked={lockWriting}
-                        key={child._id} />
-                ))}
+                </div>
+                <Droppable id="droppable" className="droppable" function={addChild}>
+                    <h3>Children:</h3>
+                    {children && children.map((child) =>
+                    (
+                        <Storynode
+                            storynodeData={child}
+                            buttonType='remove'
+                            parentFunction={updateStorynode}
+                            locked={lockWriting}
+                            key={child._id} />
+                    ))}
                 </Droppable>
+                {showModal && <DeleteConfirmation hideModal={() => setShowModal(false)} confirmModal={handleDelete} />}
             </div>
-            {showModal && <DeleteConfirmation hideModal={() => setShowModal(false)} confirmModal={handleDelete} />}
-        </div>
-        <AddSidebar kind="templates" type="root" />
+            <AddSidebar kind="templates" type="branch" />
         </>
     );
 };
