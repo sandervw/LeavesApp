ca76f02753e510d46996ed69ed82cfa95fec0144
Sander <14879109+sandervw@users.noreply.github.com>
2025-04-10 19:24:06 -0500

    fixed some small stuff

--- frontend/src/components/part/ElementFeature.jsx
+++ frontend/src/components/part/ElementFeature.jsx
@@ -1,4 +1,7 @@
 const ElementFeature = ({ element, onUpdate }) => {
+
+    const { name, kind, type, wordWeight, wordLimit, wordCount } = element;
+
     return (
         <div className="box">
             <h2
@@ -6,15 +9,39 @@ const ElementFeature = ({ element, onUpdate }) => {
                 suppressContentEditableWarning={true}
                 id={"name"}
                 onBlur={(e) => onUpdate('name', e.target.innerText)}
-            >{element.name}</h2>
-            <p>Type: {element.type}</p>
-            <p>Weight: </p>
-            <p
-                contentEditable
-                suppressContentEditableWarning={true}
-                id={"wordWeight"}
-                onBlur={(e) => onUpdate('wordWeight', e.target.innerText)}
-            >{element.wordWeight}</p>
+            >{name}
+            </h2>
+            <p>Type: {type}</p>
+            {(kind === "template" && wordWeight) && <div>
+                <p>Weight:</p>
+                <p
+                    contentEditable
+                    suppressContentEditableWarning={true}
+                    id={"wordWeight"}
+                    onBlur={(e) => onUpdate('wordWeight', e.target.innerText)}
+                >{wordWeight}
+                </p>
+            </div>}
+            {kind === "storynode" &&
+                <>
+                    {type === 'root'
+                        ? <div>
+                            <p>Word Limit: </p>
+                            <p
+                                contentEditable
+                                suppressContentEditableWarning={true}
+                                onBlur={(e) => onUpdate('wordLimit', e.target.innerText)}
+                            >{wordLimit}</p>
+                        </div>
+                        : <div>
+                            <p>Word Limit: {wordLimit}</p>
+                        </div>}
+                    {type === 'leaf' &&
+                        <div>
+                            <h3>Word count: {wordCount}</h3>
+                        </div>}
+                </>
+            }
         </div>
     );
 };
