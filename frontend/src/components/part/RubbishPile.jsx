491f6b92b42d4c331d30c6c8dc708dc6e315d356
sandervw <sam.vanwilligen@gmail.com>
2025-04-19 12:40:50 -0500

    refactored droppable delete

--- frontend/src/components/part/RubbishPile.jsx
+++ frontend/src/components/part/RubbishPile.jsx
@@ -1,52 +1,36 @@
 import React from 'react';
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import DeleteConfirmation from '../overlay/DeleteConfirmation';
 import Droppable from '../wrapper/Droppable';
-import useAPI from '../../hooks/useAPI';
-import useElementContext from '../../hooks/useElementContext';
+import useDropHandler from '../../hooks/useDropHandler';
 
 /**
  * 
  * @returns {JSX.Element} A Droppable component where elements can be dragged for deletion.
  */
-const RubbishPile = () => {    
-    const { element, dispatch } = useElementContext();
-    const navigate = useNavigate();
-    const apiCall = useAPI();
+const RubbishPile = () => {
     const [showModal, setShowModal] = useState(false);
-    const [deleteParams, setDeleteParams] = useState({ source: '', kind: '', data: null });
-
-    const handleDelete = async (confirmArgs) => {
-        // If no args are passed in, use the deleteParams stored in state
-        const { source, kind, data } = confirmArgs || deleteParams;
-        console.log('Deleting element:', source, kind, data);
-        if (['children', 'roots', 'detail'].includes(source)) {
-            await apiCall('deleteElement', kind, data._id);
-            await dispatch({ type: 'DELETE_CHILD', payload: data._id });
-            setShowModal(false);
-            if(source === 'detail') {
-                if (!element.parent) navigate('/');
-                else navigate('/storydetail', { state: element.parent });
-            }
-        } else {
-            console.error('Cannot delete element from:', source);
-            setShowModal(false);
-        }
-        setDeleteParams({ source: '', kind: '', data: null }); // Reset deleteParams after deletion
-    };
+    const [deleteParams, setDeleteParams] = useState({ source: '', data: null });
+    const { handleDelete } = useDropHandler('trash');
 
     // Make user confirm deletion if it is a detail or root element
-    const confirmDelete = (source, data) => {
-        setDeleteParams({ source, kind: `${data.kind}s`, data });
+    const checkConfirmation = (source, data) => {
+        setDeleteParams({ source, data });
         if (source==='detail' || source==='roots') setShowModal(true);
-        else handleDelete({source, kind: `${data.kind}s`, data}); // No confirmation needed for children
+        else handleDelete(source, data); // No confirmation needed for children
+    }
+
+    // handle delete on confirmation
+    const confirmDelete = () => {
+        handleDelete(deleteParams.source, deleteParams.data);
+        setDeleteParams({ source: '', data: null }); // Reset deleteParams after deletion
+        setShowModal(false);
     }
 
     return ( 
-        <Droppable id='trash' className='rubbish-box' function={confirmDelete}>
+        <Droppable id='trash' className='rubbish-box' function={checkConfirmation}>
             <p>Drag and drop here to delete</p>
-            {showModal && <DeleteConfirmation hideModal={() => setShowModal(false)} confirmModal={() => handleDelete()} />}
+            {showModal && <DeleteConfirmation hideModal={() => setShowModal(false)} confirmModal={() => confirmDelete()} />}
         </Droppable>
         
      );
--- frontend/src/hooks/useDropHandler.js
+++ frontend/src/hooks/useDropHandler.js
@@ -1,24 +1,44 @@
 import useElementContext from './useElementContext';
 import useAPI from './useAPI';
+import { useNavigate } from 'react-router-dom';
 
-const useDropHandler = (listType) => {
+/**
+ * Used by droppable components to handle when an item is dropped onto them.
+ * Add/Delete methods require a source (list), and the data (element) being dropped.
+ * 
+ * @param {string} droppableType - 'Roots', 'children', 'static', or 'trash'.
+ * @returns {object} - An object containing the handleDelete and handleAdd functions.
+ */
+const useDropHandler = (droppableType) => {
     const { element, dispatch: elementDispatch } = useElementContext();
     const apiCall = useAPI();
+    const navigate = useNavigate();
 
-    const handleDelete = () => {
-        //TODO: Implement delete functionality
+    const handleDelete = async (source, data) => {
+        if (droppableType !== 'trash') console.error('Cannot call delete from list type:', droppableType);
+        else if (['children', 'roots', 'detail'].includes(source)) {
+            console.log('Deleting element:', source, data.kind, data);
+            const kind = `${data.kind}s`
+            await apiCall('deleteElement', kind, data._id);
+            await elementDispatch({ type: 'DELETE_CHILD', payload: data._id });
+            if(source === 'detail') {
+                if (!element.parent) navigate('/');
+                else navigate('/storydetail', { state: element.parent });
+            }
+        }
+        else console.error('Cannot delete element from:', source);
     };
 
     const handleAdd = async (source, data) => {
         console.log('Adding element:', source, data);
         const kind = `${data.kind}s`
-        if (listType === 'static') return; //Prevent adding to static lists
+        if (droppableType === 'static') return; //Prevent adding to static lists
         let newChild;
-        if (listType === 'roots') {
+        if (droppableType === 'roots') {
             if (source === 'static') newChild = await apiCall('createFromTemplate', data._id, null);
             else newChild = await apiCall('upsertElement', kind, data);  // source = templateCreate or storynodeCreate
         }
-        if (listType === 'children') {
+        if (droppableType === 'children') {
             if (element.type === 'leaf') element.type = 'branch';
             await apiCall('upsertElement', kind, { ...element });
             if (source === 'static') newChild = await apiCall('createFromTemplate', data._id, element._id);
