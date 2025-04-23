bcbfca3ca46f761c1fcff6ede05d9181c7364565
Sander <14879109+sandervw@users.noreply.github.com>
2025-04-23 15:40:14 -0500

    More refactoring.

---
+++ backend/src/constants/env.ts
@@ -0,0 +1,11 @@
+const getEnv = (key: string, defaultValue?: string): string => {
+    const value = process.env[key] || defaultValue;
+    if (value === undefined) {
+        throw new Error(`Missing environment variable ${key}`);
+    }
+    return value;
+}
+
+export const PORT = getEnv("PORT", "3000");
+export const MONGO_URI = getEnv("MONGO_URI");
+export const JWT_SECRET = getEnv("JWT_SECRET");
