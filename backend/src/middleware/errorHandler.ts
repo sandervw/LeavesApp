c831f90361142a1637d73d9e7fff8335c4083096
sandervw <sam.vanwilligen@gmail.com>
2025-04-24 08:48:52 -0500

    Fixing up user logic

--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -1,11 +1,34 @@
-import { ErrorRequestHandler } from "express";
+import { ErrorRequestHandler, Response } from "express";
+import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from "../constants/http";
+import { z } from "zod";
 
 /**
- * Middleware to catch all errors throw my api routes
+ * Middleware to catch all errors throw by api routes
  */
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
     console.log(`PATH: ${req.path}`, error);
-    res.status(500).send("Internal Server Error"); // 500 is a generic server error
+
+    if (error instanceof z.ZodError) {
+        return handleZodError(res, error);
+    }
+
+    res.status(INTERNAL_SERVER_ERROR).send("Internal Server Error");
 };
+
+/**
+ * Return nicer zod error messages
+ * @param res
+ * @param error 
+ */
+const handleZodError = (res: Response, error: z.ZodError) => {
+    const errors = error.issues.map((err) => ({
+        path: err.path.join("."),
+        message: err.message,
+    }));
+    res.status(BAD_REQUEST).json({
+        message: 'Invalid request',
+        errors
+    });
+}
 
 export default errorHandler;
