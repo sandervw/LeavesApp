d8428eb0764a4402795ca5e1096381ac04c8869d
Sander <14879109+sandervw@users.noreply.github.com>
2025-04-25 12:30:26 -0500

    auth auth auth

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { signup, login, logout } from '../controllers/user.controller';
+import { signup, login, logout, refresh } from '../controllers/user.controller';
 
 const userRoutes = Router();
 
-// signup route
 userRoutes.post('/signup', signup);
 userRoutes.post('/login', login);
-userRoutes.post('/logout', logout);
+userRoutes.get('/logout', logout);
+userRoutes.get('/refresh', refresh);
+
 
 export default userRoutes;
