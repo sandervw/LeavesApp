4917ddb56cd405ae8a9c2e00bb117a9fd4c351cf
sandervw <sam.vanwilligen@gmail.com>
2025-04-26 16:09:23 -0500

    Work on resend email

---
+++ backend/src/utils/sendMail.ts
@@ -0,0 +1,19 @@
+import resend from "../config/resend";
+
+
+type Params = {
+    to: string;
+    subject: string;
+    text: string;
+    html: string;
+}
+
+export const sendMail = async ({to, subject, text, html}: Params) => {
+    return await resend.emails.send({
+        from: "onboarding@resend.dev",
+        to: "delivered@resend.dev",
+        subject,
+        text,
+        html,
+    })
+}
