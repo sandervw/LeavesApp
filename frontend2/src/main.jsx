1741d95c0b431c6f6f8989cdebc8af0cb134752c
sandervw <sam.vanwilligen@gmail.com>
2025-05-02 18:42:44 -0500

    Removed Chakra. What a fucking waste of my time.

--- frontend2/src/main.jsx
+++ frontend2/src/main.jsx
@@ -4,15 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.jsx';
 import { QueryClientProvider } from '@tanstack/react-query';
-import queryClient from './config/queryClient.js';
+import { queryClient } from './config/queryClient.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-      <ReactQueryDevtools position='bottom-right' initialIsOpen={false} />
-    </BrowserRouter>
-    </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+          <ReactQueryDevtools position='bottom-right' initialIsOpen={false} />
+        </BrowserRouter>
+      </QueryClientProvider>
   </StrictMode>,
 );
