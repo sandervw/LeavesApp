0116170e7a2ca620db6a924025fc4fd33c0bbbdc
Sander <14879109+sandervw@users.noreply.github.com>
2025-05-02 12:09:59 -0500

    work on frontend testing (Tanstack/Chakra)

---
+++ frontend2/src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+export const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to the Home page!</p>
+    </div>
+  );
+}
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  )
+}
+
+export default App
