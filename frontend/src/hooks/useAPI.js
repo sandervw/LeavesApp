6a9cce5ff4433581d7a138904dea9c187959db11
sandervw <sam.vanwilligen@gmail.com>
2025-05-05 18:56:37 -0500

    Fixed some API stuff

--- frontend/src/hooks/useAPI.js
+++ frontend/src/hooks/useAPI.js
@@ -7,38 +7,26 @@ const useAPI = () => {
     const [isPending, setIsPending] = useState(false);
     const { user, dispatch } = useAuthContext();
 
-    const tryRefreshToken = async () => {
-        const { status, data } = await apiService.authRefresh();
-        if (!status || status !== 'OK') {
-            setError(data.error);
-            setIsPending(false);
-            return 'failed';
-        }
-        return 'success';
-    }
-
     const apiCall = useCallback(async (method, ...args) => {
         setIsPending(true);
         setError(null);
         const apiMethods = {...apiService}
-        // if (!user) {
-        //     const data = await apiService.getUser();
-        //     // TODO: error handling
-        //     localStorage.setItem('user', JSON.stringify(data.user)); // save user to local storage
-        //     dispatch({ type: 'LOGIN', payload: data.user });
-        // }
+        if (!user) {
+            const data = await apiService.getUser();
+            localStorage.setItem('user', JSON.stringify(data.user));
+            dispatch({ type: 'LOGIN', payload: data.user });
+        }
         if (!apiMethods[method]) throw new Error(`Unknown API method: ${method}`);
-        // TODO: refresh token if expired
-        const result = await apiMethods[method](...args);
-        if(result.error && result.error.name==='TokenExpiredError') dispatch({ type: 'LOGOUT' });
-        if (result.error) {
-            setError(result.error);
+        try {
+            const result = await apiMethods[method](...args);
+            setError(null);
             setIsPending(false);
             return result;
+        } catch (error) {
+            setError(error);
+            setIsPending(false);
+            return;
         }
-        setError(null);
-        setIsPending(false);
-        return result;
     }, [dispatch, user]);
     return { apiCall, error, isPending };
 }
