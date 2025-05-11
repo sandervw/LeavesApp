46f9bf6b3c52fd086ffc3b064d56b73ea6620cb5
sandervw <sam.vanwilligen@gmail.com>
2025-05-11 17:08:54 -0500

    Some cleanup - added docs

--- frontend/src/config/apiClient.js
+++ frontend/src/config/apiClient.js
@@ -9,8 +9,21 @@ const options = {
   },
 };
 
-const API = axios.create(options);
+/**
+ * Used to refresh access token.
+ * Same as API, but without error interceptor.
+ * Prevents infinite loop when refreshing token.
+ */
+const TokenRefreshClient = axios.create(options);
+TokenRefreshClient.interceptors.response.use(
+  (response) => {
+    return response.data;
+  });
 
+/**
+ * Main API client.
+ */
+const API = axios.create(options);
 API.interceptors.response.use(
   (response) => {
     console.log(response);
@@ -18,21 +31,24 @@ API.interceptors.response.use(
   },
   async (error) => {
     console.log(error);
-
-    const { status, data } = error.response;
+    const { config, response } = error;
+    const { status, data } = response;
 
     // try to refresh the access token behind the scenes
     if (status === 401 && data?.errorCode === "InvalidAccessToken") {
       try {
-        // refresh the access token, then retry the original request
-        await API.get("/auth/refresh");
-        const user = await API.get("/user");
+        // refresh the access token (and user context)
+        await TokenRefreshClient.get("/auth/refresh");
+        const user = await TokenRefreshClient.get("/user");
         if (user) { // trigger user context update
           window.dispatchEvent(new CustomEvent('userUpdated', { detail: user }));
         }
-        // return TokenRefreshClient(config);
+        // retry the original request
+        config._retry = true;
+        return API(config);
       } catch (refreshError) {
         console.log("Token refresh failed", refreshError);
+        // Remove user from context
         window.dispatchEvent(new CustomEvent('userUpdated', { detail: null }));
       }
     }
