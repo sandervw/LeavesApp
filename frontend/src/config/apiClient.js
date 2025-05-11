07fe17341f99565c89c0843a64cccf7c79593596
sandervw <sam.vanwilligen@gmail.com>
2025-05-11 12:36:59 -0500

    Finally got auth mostly working

--- frontend/src/config/apiClient.js
+++ frontend/src/config/apiClient.js
@@ -1,44 +1,43 @@
 import axios from 'axios';
-import { setUser } from './authClient';
 
 const options = {
-    baseURL: import.meta.env.VITE_BASEAPIURL,
-    withCredentials: true,
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    },
+  baseURL: import.meta.env.VITE_BASEAPIURL,
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  },
 };
 
 const API = axios.create(options);
 
 API.interceptors.response.use(
-    (response) => {
-        console.log(response);
-        return response.data;
-    },
-    async (error) => {
-      console.log(error);
-      
-      const { status, data } = error.response;
-      
-      // try to refresh the access token behind the scenes
-      if (status === 401 && data?.errorCode === "InvalidAccessToken") {
-        try {
-          // refresh the access token, then retry the original request
-          await API.get("/auth/refresh");
-          const user = await API.get("/user");
-          if (user) {
-            setUser(user);
-          }
-          // return TokenRefreshClient(config);
-        } catch (refreshError) {
-          console.log("Token refresh failed", refreshError);
-          setUser(null); // Trigger logout in react context
+  (response) => {
+    console.log(response);
+    return response.data;
+  },
+  async (error) => {
+    console.log(error);
+
+    const { status, data } = error.response;
+
+    // try to refresh the access token behind the scenes
+    if (status === 401 && data?.errorCode === "InvalidAccessToken") {
+      try {
+        // refresh the access token, then retry the original request
+        await API.get("/auth/refresh");
+        const user = await API.get("/user");
+        if (user) { // trigger user context update
+          window.dispatchEvent(new CustomEvent('userUpdated', { detail: user }));
         }
+        // return TokenRefreshClient(config);
+      } catch (refreshError) {
+        console.log("Token refresh failed", refreshError);
+        window.dispatchEvent(new CustomEvent('userUpdated', { detail: null }));
       }
-      return Promise.reject(`Error: ${data.message|| "unknown error"} `);
     }
+    return Promise.reject(`Error: ${data.message || "unknown error"} `);
+  }
 );
 
 export default API;
