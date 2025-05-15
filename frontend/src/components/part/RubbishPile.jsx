d5370e70e4be3fd994584c416f2157052805c878
sandervw <sam.vanwilligen@gmail.com>
2025-05-15 13:28:21 -0500

    Got rubbish css/function "working" (not well-designed)

--- frontend/src/components/part/RubbishPile.jsx
+++ frontend/src/components/part/RubbishPile.jsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useDndContext } from '@dnd-kit/core';
 import DeleteConfirmation from '../overlay/DeleteConfirmation';
 import Droppable from '../wrapper/Droppable';
 import useDropHandler from '../../hooks/useDropHandler';
 import InlineSVG from './common/InlineSVG';
 
 /**
- * 
  * @returns {JSX.Element} A Droppable component where elements can be dragged for deletion.
  */
 const RubbishPile = () => {
     const [showModal, setShowModal] = useState(false);
     const [deleteParams, setDeleteParams] = useState({ source: '', data: null });
     const { handleDelete } = useDropHandler('trash');
+    const { active } = useDndContext();
+    const [isDeleteable, setIsDeleteable] = useState(false);
+    const [className, setClassName] = useState('rubbish-pile');
+    useEffect(() => {
+        console.log('RubbishPile active:', active);
+        setIsDeleteable(false); // Reset isDeleteable state
+        setClassName('rubbish-pile'); // Reset className state
+        // Check if the active item is a droppable element
+        if(active
+        && (active.data.current.source === 'detail'
+            || active.data.current.source === 'roots'
+            || active.data.current.source === 'children'
+        )){
+            setIsDeleteable(true);
+            setClassName('rubbish-pile active');
+        }
+    }, [active]);
 
     // Make user confirm deletion if it is a detail or root element
     const checkConfirmation = (source, data) => {
@@ -29,9 +45,10 @@ const RubbishPile = () => {
     }
 
     return ( 
-        <Droppable id='trash' className='trash' function={checkConfirmation}>
-            <InlineSVG src='/trashcan.svg' alt='trashcan icon' className='icon' />
-            <p>Drag and drop here to delete</p>
+        <Droppable id='trash' className={className} function={checkConfirmation}>
+            {isDeleteable
+            ? <p>Drag and drop here to delete</p>
+            : <InlineSVG src='/trashcan.svg' alt='trashcan icon' className='icon' />}
             {showModal && <DeleteConfirmation hideModal={() => setShowModal(false)} confirmModal={() => confirmDelete()} />}
         </Droppable>
         
