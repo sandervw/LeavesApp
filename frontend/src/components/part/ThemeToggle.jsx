426c6640ce63f11cab26ec1d4abbcb22053d369e
Sander <14879109+sandervw@users.noreply.github.com>
2025-10-29 17:04:25 -0500

    Moved ThemeToggle to Navbar.

--- frontend/src/components/part/ThemeToggle.jsx
+++ frontend/src/components/part/ThemeToggle.jsx
@@ -2,10 +2,10 @@ import { MoonButton, SunButton } from "./common/Buttons";
 
 export const ThemeToggle = ({ theme, setTheme }) => {
     return (
-        <div className='theme-toggle'>
+        <>
             {theme==='light'
             ? <MoonButton onClick={() => setTheme('dark')} />
             : <SunButton onClick={() => setTheme('light')} />}
-        </div>
+        </>
     );
 };
