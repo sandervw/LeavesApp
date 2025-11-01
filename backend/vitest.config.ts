eb3eb38af48dacee09c7e3b8927c33187fb71b6b
sandervw <sam.vanwilligen@gmail.com>
2025-10-31 19:07:13 -0500

    Added test-specific config file.

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -6,6 +6,9 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     setupFiles: ['./tests/setup.ts'],
+    typecheck: {
+      tsconfig: './tsconfig.test.json',
+    },
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
