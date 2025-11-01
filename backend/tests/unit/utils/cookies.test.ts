26dc5844686a804bd5e39b70dfa6e41a0e1a90cf
Sander <14879109+sandervw@users.noreply.github.com>
2025-11-01 17:15:21 -0500

    Work on cookies.test.ts

--- backend/tests/unit/utils/cookies.test.ts
+++ backend/tests/unit/utils/cookies.test.ts
@@ -2,8 +2,44 @@ import { describe, it, expect } from 'vitest';
 import { setAuthCookies, clearAuthCookies, getRefreshTokenCookieOptions, getAccessTokenCookieOptions } from '../../../src/utils/cookies';
 
 describe('Cookies utils', () => {
-  describe('Auth cookies', () => {
+  describe('setAuthCookies', () => {
+    it('Should only set accessToken if no refreshToken provided', () => {
+      const mockRes: Response = {};
+    });
 
+    it('Should set both cookies when when refreshToken is provided', () => {
+      const mockRes: Response = {};
+    });
+
+    it('Should call res.cookie with the correct cookie names', () => {
+      const mockRes: Response = {};
+    });
+
+    it('Should use correct cookie type options', () => {
+      const mockRes: Response = {};
+    });
+
+    it('Should return the response object for chaining', () => {
+      const mockRes: Response = {};
+    });
+  });
+
+  describe('clearAuthCookies', () => {
+    it('', () => {
+      const mockRes: Response = {};
+    });
+  });
+
+  describe('getAccessTokenCookieOptions', () => {
+    it('', () => {
+      const mockRes: Response = {};
+    });
+  });
+
+  describe('getRefreshTokenCookieOptions', () => {
+    it('', () => {
+      const mockRes: Response = {};
+    });
   });
 }
 );
